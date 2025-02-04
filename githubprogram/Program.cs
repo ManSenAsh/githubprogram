7018aad33599ec09a1771dc8e2744fe8960ce837
twist <twist@MANSENASH>
2025-02-04 11:14:09 -0600

    Agregar archivos de proyecto.

---
+++ githubprogram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mmundo banda");
+            Console.ReadLine();
+        }
+    }
+}
